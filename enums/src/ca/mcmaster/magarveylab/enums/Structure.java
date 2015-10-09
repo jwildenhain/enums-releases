71b5791528972445c45f859fabc3dd4a084aca17
stlang <chris.stlang@gmail.com>
2015-10-09 11:31:47 -0400

    removed unusued

--- enums/src/ca/mcmaster/magarveylab/enums/Structure.java
+++
@@ -1,7 +0,0 @@
-package ca.mcmaster.magarveylab.enums;
-
-public interface Structure {
-
-	public String smiles();
-	
-}
--- enums/src/ca/mcmaster/magarveylab/enums/clusters/ClusterType.java
+++
@@ -1,7 +0,0 @@
-package ca.mcmaster.magarveylab.enums.clusters;
-
-public interface ClusterType {
-
-	public String fullName();
-	
-}
